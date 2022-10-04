c699e082c3bdeee666fc9b68ebda69f4082eaac3
Maksim Mezhuev <mezhuevmaksim3@gmail.com>
2022-10-04 20:35:57 +0300

    исправлен -0

--- TrainingSolution/TrainingProject_DivOperation/div_operation.c
+++ TrainingSolution/TrainingProject_DivOperation/div_operation.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <locale.h>
 int main() {
-	setlocale(LC_ALL, "russia");
+	setlocale(LC_ALL, "russian");
 	int a, b;
 	float div;
 	scanf_s("%d %d", &a, &b);
 	if (b == 0) {
-		printf("Плохо");
+		printf("Плохо\n");
 	}
 	else {
-		div = a / b;
-		printf("%f", div);
-    }
+		if (a == 0) {
+			printf("0");
+		}
+		else {
+			div = a / b;
+			printf("%f", div);
+		}
+	}
 	system("pause");
 	return 0;
 }
