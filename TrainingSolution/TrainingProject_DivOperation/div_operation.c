c1862a6e1d06a58bb149ba8e9bdb9d25abdea063
Maksim Mezhuev <mezhuevmaksim3@gmail.com>
2022-10-04 20:31:38 +0300

    исправлен баг с нулем

--- TrainingSolution/TrainingProject_DivOperation/div_operation.c
+++ TrainingSolution/TrainingProject_DivOperation/div_operation.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <locale.h>
 int main() {
+	setlocale(LC_ALL, "russia");
 	int a, b;
 	float div;
 	scanf_s("%d %d", &a, &b);
-	div = a / b;
-	printf("%f", div);
+	if (b == 0) {
+		printf("Плохо");
+	}
+	else {
+		div = a / b;
+		printf("%f", div);
+    }
 	system("pause");
 	return 0;
 }
